a71174f5c2b24c1524ddd4bac0a627fed714fd7d
Miguel Ardon <miguel.ardonenriquez@gmail.com>
2022-06-09 11:23:38 -0600

    changing to hooks

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,33 +1,27 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Modal from '../components/Modal';
 
-class Card extends Component {
-    constructor() {
-        super();
-        this.state = {
-            show: false
-        }
+function Card({ name, level, photo }) {
+    const [show, setShow] = useState(false);
+
+    const showModal = () => {
+        setShow(true);
     }
-    showModal = () => {
-        console.log('entra')
-        this.setState({ show: true });
+
+    const hideModal = () => {
+        setShow(false);
     }
-    hideModal = () => {
-        console.log('closed')
-        this.setState({ show: false });
-    }
-    render() {
-        return (
-            <div className='tc bg-dark-blue dib br3 pa3 ma2 grow bw2 shadow-5' >
-                <img src={`${this.props.photo}`} alt='_digiPhoto_' onClick={this.showModal} />
-                <div>
-                    <h2>{this.props.name}</h2>
-                    <p>{this.props.level}</p>
-                </div>
-                <Modal show={this.state.show} handleClose={this.hideModal} img={this.props.photo} />
+
+    return (
+        <div className='tc bg-dark-blue dib br3 pa3 ma2 grow bw2 shadow-5' >
+            <Modal show={show} handleClose={hideModal} img={photo} />
+            <img src={`${photo}`} alt='_digiPhoto_' onClick={showModal} />
+            <div>
+                <h2>{name}</h2>
+                <p>{level}</p>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default Card;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,48 +1,42 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from '../components/CardList';
 import SearchCard from '../components/SearchCard';
 import Scroll from '../components/Scroll';
 import './App.css'
 
-class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            digimons: [],
-            digisearch: ''
-        }
-    }
-    componentDidMount() {
+function App() {
+    const [digimons, setDigimons] = useState([]);
+    const [digisearch, setDigisearch] = useState('');
+
+    useEffect(() => {
         fetch('https://digimon-api.vercel.app/api/digimon').then(response => {
             return response.json();
         }).then(digi => {
-            this.setState({ digimons: digi });
+            setDigimons(digi);
         })
-    }
-    onSearchCard = (event) => {
-        this.setState({ digisearch: event.target.value });
-    }
-    render() {
-        const digimonFilter = this.state.digimons.filter(digimon => {
-            return digimon.name.toLowerCase().includes(this.state.digisearch.toLowerCase());
-        })
-        if (this.state.digimons.length === 0) {
-            return (
-                <h1>Loading...</h1>
-            )
-        } else {
-            return (
-                <div className="tc">
-                    <h1 className="f1">Digimon Cards</h1>
-                    <SearchCard searchCard={this.onSearchCard} />
-                    <Scroll>
-                        <CardList digimons={digimonFilter} />
-                    </Scroll>
-                </div>
-            );
-        }
+    }, [])
 
+    const onSearchCard = (event) => {
+        setDigisearch(event.target.value);
+    }
 
+    const digimonFilter = digimons.filter(digimon => {
+        return digimon.name.toLowerCase().includes(digisearch.toLowerCase());
+    })
+    if (digimons.length === 0) {
+        return (
+            <h1>Loading...</h1>
+        )
+    } else {
+        return (
+            <div className="tc">
+                <h1 className="f1">Digimon Cards</h1>
+                <SearchCard searchCard={onSearchCard} />
+                <Scroll>
+                    <CardList digimons={digimonFilter} />
+                </Scroll>
+            </div >
+        );
     }
 }
 
